947bc1597362cadf35277ff33e816d517397e5ad
NehalDutta-7002089125 <118413398+NehalDutta-7002089125@users.noreply.github.com>
2022-11-21 08:56:16 +0530

    Update DeclareNum.sh

--- DeclareNum.sh
+++ DeclareNum.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -x
-declare -A num1
 read -a "Enter first number:" num1
-declare -A num2
 read -a "Enter second number:" num2
 if [ $num1 -gt $num2 ]
 then
@@ -9,5 +7,3 @@ echo "$num1 is greater"
 else
 echo "$num2 is smaller"
 fi
-#b=( $num1, $num2 )
-#echo {b[@]}
